27d987efc3e895a53391eed3b66128e36213e02a
JethroNatividad <natividad.jet@gmail.com>
2024-03-04 23:55:53 +0800

    create sign up

--- app/signUp/page.tsx
+++ app/signUp/page.tsx
@@ -1,17 +1,89 @@
 "use client";
-
-import { auth } from "../../firebase";
+// pages/SignIn.tsx
 import React from "react";
-import { useAuthState } from "react-firebase-hooks/auth";
+import {
+  LiteralUnion,
+  RegisterOptions,
+  SubmitHandler,
+  useForm,
+} from "react-hook-form";
+import { auth } from "../../firebase";
+import { createUserWithEmailAndPassword } from "firebase/auth";
+
+type Inputs = {
+  email: string;
+  password: string;
+};
+
+type ErrorOptionTypes = LiteralUnion<keyof RegisterOptions, string>;
+
+type ErrorMessages = {
+  [K in keyof Inputs]: {
+    [T in ErrorOptionTypes]?: string;
+  };
+};
+
+const errorMessages: ErrorMessages = {
+  email: {
+    required: "Email is required",
+    pattern: "Email must end in @wmsu.edu.ph",
+  },
+  password: {
+    required: "Password is required",
+  },
+};
 
 const SignUp = () => {
-  const [user, loading, error] = useAuthState(auth);
+  const {
+    register,
+    handleSubmit,
+    setError,
+    formState: { errors },
+  } = useForm<Inputs>();
 
-  if (loading) return <div>Loading...</div>;
-  if (error) return <div>Error!</div>;
-  if (!user) return <div>You must be signed in to complete the sign-up.</div>;
+  const onSubmit: SubmitHandler<Inputs> = async ({ email, password }) => {
+    try {
+      const userCredential = await createUserWithEmailAndPassword(
+        auth,
+        email,
+        password
+      );
+    } catch (error) {
+      setError("root", {
+        type: "server",
+        message: (error as Error).message,
+      });
+    }
+  };
 
-  return <div>Sign-up form goes here, welcome {user.email}</div>;
+  return (
+    <div>
+      <form onSubmit={handleSubmit(onSubmit)}>
+        <input
+          type="email"
+          placeholder="eh123456789@wmsu.edu.ph"
+          {...register("email", {
+            required: true,
+            pattern: /^[a-zA-Z0-9._%+-]+@wmsu\.edu\.ph$/,
+          })}
+        />
+        {errors.email && <p>{errorMessages.email[errors.email?.type]}</p>}
+
+        <input
+          type="password"
+          placeholder="Password"
+          {...register("password", { required: true })}
+        />
+
+        {errors.password && (
+          <p>{errorMessages.password[errors.password?.type]}</p>
+        )}
+
+        {errors.root?.type == "server" && <p>{errors.root.message}</p>}
+        <button type="submit">Sign in</button>
+      </form>
+    </div>
+  );
 };
 
 export default SignUp;
