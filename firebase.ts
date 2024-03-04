d3058cfa4d3f0df3c9a6f18a2e69b3f0e1f43213
JethroNatividad <natividad.jet@gmail.com>
2024-03-04 21:54:07 +0800

    firebase: export firestore

--- firebase.ts
+++ firebase.ts
@@ -1,5 +1,6 @@
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
   apiKey: "AIzaSyBil6vfsiSA75fm9jd4O6fogIF_ZE-x3CY",
@@ -13,3 +14,4 @@ const firebaseConfig = {
 
 const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app);
+export const firestore = getFirestore(app);
