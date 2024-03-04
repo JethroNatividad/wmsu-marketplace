4414862f6e34797f8c6ba06c8e8caf3c76957be1
JethroNatividad <natividad.jet@gmail.com>
2024-03-04 21:51:10 +0800

    cleanup firebase

--- firebase.ts
+++ firebase.ts
@@ -1,12 +1,6 @@
-// Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
 import { getAuth } from "firebase/auth";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
 
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
   apiKey: "AIzaSyBil6vfsiSA75fm9jd4O6fogIF_ZE-x3CY",
   authDomain: "wmsumarketplace.firebaseapp.com",
@@ -17,6 +11,5 @@ const firebaseConfig = {
   measurementId: "G-MBZM5WGZB9",
 };
 
-// Initialize Firebase
 const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app);
