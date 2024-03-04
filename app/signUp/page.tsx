bb596469aabd135fb86d1387185716f459484ec2
JethroNatividad <natividad.jet@gmail.com>
2024-03-04 23:10:01 +0800

    create sign up page

---
+++ app/signUp/page.tsx
@@ -0,0 +1,15 @@
+import { auth } from "../../firebase";
+import React from "react";
+import { useAuthState } from "react-firebase-hooks/auth";
+
+const SignUp = () => {
+  const [user, loading, error] = useAuthState(auth);
+
+  if (loading) return <div>Loading...</div>;
+  if (error) return <div>Error!</div>;
+  if (!user) return <div>You must be signed in to complete the sign-up.</div>;
+
+  return <div>Sign-up form goes here, welcome {user.email}</div>;
+};
+
+export default SignUp;
