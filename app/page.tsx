900a333e5760a1c7b053340120dbfe7c992cbc4c
JethroNatividad <natividad.jet@gmail.com>
2024-03-05 12:30:54 +0800

    homepage: implement checks

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,31 @@
 "use client";
 
 import useUserState from "@/hooks/useUserState";
+import { useRouter } from "next/navigation";
+import { useEffect } from "react";
 
 const Home = () => {
   const { user, userData, loading, error } = useUserState();
+  const router = useRouter();
+
+  useEffect(() => {
+    // - Not logged in ? redirect to /signIn : continue
+    // - user not yet verified ? redirect to /verifyEmail: continue
+    // - User not yetcompleteSignUp ? redirect to /completeSignUp  : continue
+    if (!loading && !error) {
+      if (!user || !userData) {
+        return router.push("/signIn");
+      }
+
+      if (!user.emailVerified) {
+        return router.push("/verifyEmail");
+      }
+
+      if (!userData.completeSignUp) {
+        return router.push("/completeSignUp");
+      }
+    }
+  }, [user, loading, error]);
 
   if (loading) {
     return <div>Loading...</div>;
