b3998f17f513e6cd202cbb8de4e1066ea4373bc5
JethroNatividad <natividad.jet@gmail.com>
2024-03-05 22:59:02 +0800

    Create Item Model

---
+++ models/Item.ts
@@ -0,0 +1,24 @@
+import { UserInfo } from "firebase/auth";
+
+// Item:
+// seller: sellerId
+// Item name: string
+// price: number
+// discount: number
+// category:
+// condition: New, used - like new, used - good, used - fair
+// Description: string
+// campus: Main Campus A, Main Campus B
+// tags: [string]
+
+export interface Item {
+  seller: UserInfo["uid"];
+  itemName: string;
+  price: number;
+  discount: number;
+  category: string;
+  condition: string;
+  description: string;
+  campus: string;
+  tags: string[];
+}
