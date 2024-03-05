b3df195aa9574ad06fa8c45d8e24db32f6f1fdae
JethroNatividad <natividad.jet@gmail.com>
2024-03-05 14:46:01 +0800

    Style verifyEmail

--- app/verifyEmail/page.tsx
+++ app/verifyEmail/page.tsx
@@ -5,6 +5,8 @@ import { useRouter } from "next/navigation";
 import { sendEmailVerification } from "firebase/auth";
 import { auth } from "@/firebase";
 import { useAuthState } from "react-firebase-hooks/auth";
+import authHeroImage from "@/assets/images/auth-hero.png";
+import Image from "next/image";
 
 const VerifyEmail = () => {
   const [user, loading, error] = useAuthState(auth);
@@ -34,12 +36,23 @@ const VerifyEmail = () => {
   }
 
   return (
-    <div>
-      <div>
-        <p>Verify Email</p>
-        <button onClick={handleSendEmailVerification}>
-          Send Verification Email
-        </button>
+    <div className="flex h-screen">
+      <div className="hidden lg:block w-1/2 h-full relative overflow-hidden">
+        <Image className="object-cover" fill src={authHeroImage} alt="hero" />
+      </div>
+      <div className="w-full lg:w-1/2 h-full flex lg:items-center justify-center">
+        <div className="w-full max-w-lg px-5 py-10 lg:py-0 space-y-3">
+          <h1 className="text-4xl font-bold">Verify Your Email</h1>
+          <p>
+            Account Created Successfully. Please verify your email to continue.
+          </p>
+          <button
+            className="btn btn-primary"
+            onClick={handleSendEmailVerification}
+          >
+            Send Verification Email
+          </button>
+        </div>
       </div>
     </div>
   );
