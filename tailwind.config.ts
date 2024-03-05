6df29986f4012391432024937ad13f8d8b0e0bf1
JethroNatividad <natividad.jet@gmail.com>
2024-03-05 13:26:32 +0800

    install daisyUi

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,20 +1,29 @@
 import type { Config } from "tailwindcss";
 
 const config: Config = {
+  daisyui: {
+    themes: [
+      {
+        mytheme: {
+          primary: "#881337",
+          secondary: "#881337",
+          accent: "#6b7280",
+          neutral: "#881337",
+          "base-100": "#e5e7eb",
+          info: "#1e3a8a",
+          success: "#3f6212",
+          warning: "#92400e",
+          error: "#881337",
+        },
+      },
+    ],
+  },
+  plugins: [require("daisyui")],
   content: [
     "./pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./components/**/*.{js,ts,jsx,tsx,mdx}",
     "./app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
-  theme: {
-    extend: {
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
-      },
-    },
-  },
-  plugins: [],
+  theme: {},
 };
 export default config;
