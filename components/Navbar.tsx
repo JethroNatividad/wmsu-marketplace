902132c4b6db0c3cd4a91b3d1887dd512af10c87
JethroNatividad <natividad.jet@gmail.com>
2024-03-05 23:10:18 +0800

    Style navbar

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -2,18 +2,11 @@ import React from "react";
 
 const Navbar = () => {
   return (
-    <header className="navbar bg-base-100">
+    <header className="navbar bg-base-100 shadow-md">
       <div className="flex-1">
-        <a className="btn btn-ghost text-xl">daisyUI</a>
+        <a className="btn btn-ghost text-xl">WMSU Marketplace</a>
       </div>
       <div className="flex-none gap-2">
-        <div className="form-control">
-          <input
-            type="text"
-            placeholder="Search"
-            className="input input-bordered w-24 md:w-auto"
-          />
-        </div>
         <div className="dropdown dropdown-end">
           <div
             tabIndex={0}
