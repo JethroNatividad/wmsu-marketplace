8a9bfa45cb7de4f9bc0c7cfe2b2b8a206be2cd3d
JethroNatividad <natividad.jet@gmail.com>
2024-03-06 01:53:02 +0800

    create pageloading

---
+++ components/PageLoading.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const PageLoading = () => {
+  return (
+    <div className="w-full h-screen flex items-center justify-center">
+      <span className="loading loading-spinner text-primary"></span>
+    </div>
+  );
+};
+
+export default PageLoading;
