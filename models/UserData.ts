eeb2fc0344f598a962306d69df1d1a019776f91f
JethroNatividad <natividad.jet@gmail.com>
2024-03-05 12:23:15 +0800

    Add user interface

---
+++ models/UserData.ts
@@ -0,0 +1,11 @@
+export interface UserData {
+  completeSignUp: boolean;
+  email: string;
+  firstName: string;
+  middleName?: string;
+  lastName: string;
+  department: string;
+  preferredCampus?: string;
+  profilePicture?: string;
+  bio?: string;
+}
