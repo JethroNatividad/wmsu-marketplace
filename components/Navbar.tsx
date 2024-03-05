c9453e741c66b5e1947716101a033a467a4a141b
JethroNatividad <natividad.jet@gmail.com>
2024-03-05 23:18:08 +0800

    Add responsive sidebar

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -2,44 +2,86 @@ import React from "react";
 
 const Navbar = () => {
   return (
-    <header className="navbar bg-base-100 shadow-md">
-      <div className="flex-1">
-        <a className="btn btn-ghost text-xl">WMSU Marketplace</a>
-      </div>
-      <div className="flex-none gap-2">
-        <div className="dropdown dropdown-end">
-          <div
-            tabIndex={0}
-            role="button"
-            className="btn btn-ghost btn-circle avatar"
-          >
-            <div className="w-10 rounded-full">
-              <img
-                alt="Tailwind CSS Navbar component"
-                src="https://daisyui.com/images/stock/photo-1534528741775-53994a69daeb.jpg"
-              />
-            </div>
+    <>
+      <header className="navbar bg-base-100 shadow-md">
+        <div className="flex-1">
+          <div className="flex-none lg:hidden">
+            <label
+              htmlFor="sidebar"
+              aria-label="open sidebar"
+              className="btn btn-square btn-ghost"
+            >
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                className="inline-block w-6 h-6 stroke-current"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth="2"
+                  d="M4 6h16M4 12h16M4 18h16"
+                ></path>
+              </svg>
+            </label>
           </div>
-          <ul
-            tabIndex={0}
-            className="mt-3 z-[1] p-2 shadow menu menu-sm dropdown-content bg-base-100 rounded-box w-52"
-          >
+          <a className="btn btn-ghost text-xl">WMSU Marketplace</a>
+        </div>
+        <div className="flex-none gap-2">
+          <div className="dropdown dropdown-end">
+            <div
+              tabIndex={0}
+              role="button"
+              className="btn btn-ghost btn-circle avatar"
+            >
+              <div className="w-10 rounded-full">
+                <img
+                  alt="Tailwind CSS Navbar component"
+                  src="https://daisyui.com/images/stock/photo-1534528741775-53994a69daeb.jpg"
+                />
+              </div>
+            </div>
+            <ul
+              tabIndex={0}
+              className="mt-3 z-[1] p-2 shadow menu menu-sm dropdown-content bg-base-100 rounded-box w-52"
+            >
+              <li>
+                <a className="justify-between">
+                  Profile
+                  <span className="badge">New</span>
+                </a>
+              </li>
+              <li>
+                <a>Settings</a>
+              </li>
+              <li>
+                <a>Logout</a>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </header>
+      <div className="drawer lg:drawer-open">
+        <input id="sidebar" type="checkbox" className="drawer-toggle" />
+        <div className="drawer-side">
+          <label
+            htmlFor="sidebar"
+            aria-label="close sidebar"
+            className="drawer-overlay"
+          ></label>
+          <ul className="menu p-4 w-80 min-h-full bg-base-200 text-base-content">
+            {/* Sidebar content here */}
             <li>
-              <a className="justify-between">
-                Profile
-                <span className="badge">New</span>
-              </a>
+              <a>Sidebar Item 1</a>
             </li>
             <li>
-              <a>Settings</a>
-            </li>
-            <li>
-              <a>Logout</a>
+              <a>Sidebar Item 2</a>
             </li>
           </ul>
         </div>
       </div>
-    </header>
+    </>
   );
 };
 
