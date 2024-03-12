7f753ccfe4ac63c92e990f70503bb0b0e399277b
JethroNatividad <natividad.jet@gmail.com>
2024-03-12 23:31:25 +0800

    Implement auth store

--- store/auth.tsx
+++ store/auth.tsx
@@ -1,13 +1,61 @@
 "use client";
 
-import React, { createContext, useState } from "react";
+import useUserState from "@/hooks/useUserState";
+import { UserData } from "@/models/User";
+import { User } from "firebase/auth";
+import { useRouter } from "next/navigation";
+import React, { createContext, useEffect } from "react";
 
-type AuthContextType = {};
+type AuthContextType = {
+  userData: UserData | null;
+  user: User | null | undefined;
+  loading: boolean;
+};
 
-const AuthContext = createContext<AuthContextType>({});
+const AuthContext = createContext<AuthContextType>({
+  userData: null,
+  user: null,
+  loading: true,
+});
 
 export const AuthProvider = ({ children }: React.PropsWithChildren) => {
-  return <AuthContext.Provider value={{}}>{children}</AuthContext.Provider>;
+  const { user, userData, loading, error } = useUserState();
+  const router = useRouter();
+
+  useEffect(() => {
+    // - Not logged in ? redirect to /signIn : continue
+    // - user not yet verified ? redirect to /verifyEmail: continue
+    // - User not yetcompleteSignUp ? redirect to /completeSignUp  : continue
+    if (!loading && !error) {
+      if (!user || !userData) {
+        return router.push("/signIn");
+      }
+
+      if (!user.emailVerified) {
+        return router.push("/verifyEmail");
+      }
+
+      if (!userData.completeSignUp) {
+        return router.push("/completeSignUp");
+      }
+    }
+
+    console.log("user", user);
+    console.log("userData", userData);
+    console.log("loading", loading);
+  }, [user, loading, error, userData]);
+
+  return (
+    <AuthContext.Provider
+      value={{
+        userData,
+        user,
+        loading,
+      }}
+    >
+      {children}
+    </AuthContext.Provider>
+  );
 };
 
 export default AuthContext;
