93038d05153e0f172ba01c313cac12301a6ce52b
JethroNatividad <natividad.jet@gmail.com>
2024-03-17 21:10:24 +0800

    use app provider

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import { AuthProvider } from "@/store/auth";
+import { AppProvider } from "@/store/app";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -18,7 +19,9 @@ export default function RootLayout({
   return (
     <html lang="en">
       <AuthProvider>
-        <body className={inter.className}>{children}</body>
+        <AppProvider>
+          <body className={inter.className}>{children}</body>
+        </AppProvider>
       </AuthProvider>
     </html>
   );
